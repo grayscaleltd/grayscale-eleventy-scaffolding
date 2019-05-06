9e35ad703abf30378e92b0d4fe63c7417a7f285a
Aceler Chua <aceler.chua@grayscale.com.hk>
2019-05-06 14:35:18 +0800

    initial commit, has no eleventy template yet

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function(config) {
+
+  // A useful way to reference to the contect we are runing eleventy in
+  let env = process.env.ELEVENTY_ENV;
+
+  // minify the html output
+  config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
+
+
+  // pass some assets right through
+  config.addPassthroughCopy("./src/site/assets");
+
+  return {
+    dir: {
+      input: "src/site",
+      output: "dist",
+      data: `_data/${env}`
+    },
+    templateFormats : ["html", "liquid", "md"],
+    passthroughFileCopy: true
+  };
+
+};
